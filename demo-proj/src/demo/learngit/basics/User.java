e4fbed90fb9b31d67e5c4e4fcbe2d4e5f49b9218
Guy Tordjman <turgibot@gmail.com>
2022-03-23 11:37:59 +0200

    add lastname field to User

--- demo-proj/src/demo/learngit/basics/User.java
+++ demo-proj/src/demo/learngit/basics/User.java
@@ -2,9 +2,11 @@ package demo.learngit.basics;
 
 public class User {
 	protected String name;
+	protected String lastName;
 
-	public User(String name) {
+	public User(String name, String lastName) {
 		this.name = name;
+		this.lastName = lastName;
 	}
 	
 }
