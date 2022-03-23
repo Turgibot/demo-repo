523a48a024ee4e410c16a7ba86673bd8a3a7e491
Guy Tordjman <turgibot@gmail.com>
2022-03-23 11:24:11 +0200

    add id field to User class

--- demo-proj/src/demo/learngit/basics/User.java
+++ demo-proj/src/demo/learngit/basics/User.java
@@ -2,9 +2,11 @@ package demo.learngit.basics;
 
 public class User {
 	protected String name;
+	protected int id;
 
-	public User(String name) {
+	public User(String name, int id) {
 		this.name = name;
+		this.id = id;
 	}
 	
 }
